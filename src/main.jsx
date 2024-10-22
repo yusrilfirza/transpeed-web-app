e402cccd3f13586e86f7a15b44f5a714bb8513b7
Firza <firza.yusril@gmail.com>
2024-10-22 22:47:25 +0700

    Set up initial code base

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import '@mantine/core/styles.css';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { createTheme, MantineProvider } from '@mantine/core';
+import App from './pages';
+import './index.css'
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <MantineProvider theme={theme}>
+      <App />
+    </MantineProvider>
+  </StrictMode>,
+)
---
+++ src/pages/index.jsx
@@ -0,0 +1,16 @@
+import { Button } from '@mantine/core';
+import Header from '../shared/component/header';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <Button variant="filled">Button</Button>
+    </div>
+  )
+}
+
+export default App
